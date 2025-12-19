d06b93e073ba9ef254cb39ae8a2a083805c33553
Artur <safiullin12artur@gmail.com>
2025-12-19 23:23:08 +0300

    final

--- scripts/like.js
+++ scripts/like.js
@@ -13,15 +13,17 @@ const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.onclick = (event) => {
     event.preventDefault();
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  };
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => 
+  button.onclick = (event) => {
     event.preventDefault();
     toggleIsLiked(likeHeartArray[index], button);
+  };
 });
 
 function toggleIsLiked(heart, button) {
